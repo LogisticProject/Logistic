a0eeab5fb1b1cabbd498c5012dda6213e23da8d0
fengli738 <fli19@hawk.iit.edu>
2020-02-24 12:29:43 -0500

    Update For API part
    API create with .net framework

---
+++ LogiApi/LogiApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace LogiApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ LogiApi/LogiApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LogiApi.WebApiApplication" Language="C#" %>
